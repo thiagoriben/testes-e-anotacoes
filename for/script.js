f4c029ffd34d5259c2386e9ffe6a9f156195891b
Thiago Ribeiro Negreiros <thiagoriben@gmail.com>
2023-12-18 11:10:58 -0300

    ex 3 com tabuada

--- for/script.js
+++ for/script.js
@@ -6,7 +6,16 @@
 }*/
 
 
-for (let contador = 0; contador <= 5; contador++){
+/*for (let contador = 0; contador <= 5; contador++){
     console.log(contador) // = 0, 1, 2, 3, 4, 5
+}*/
+
+//tabuada
+
+let tabuada = Number(prompt('escolha a tabuada que deseja.'))
+
+for (let contador = 1; contador <= 10; contador++){
+    console.log(`${tabuada} x ${contador} = ${tabuada*contador}`)
 }
 
+//ele vai multiplicar a variavel tabuada com a variavel contador, q é de 1 a 10
