e6130ba837166fdfa2fe75c398212466d336e23d
Thiago Ribeiro Negreiros <thiagoriben@gmail.com>
2023-12-18 10:23:56 -0300

    revendo switch case

---
+++ switch case/script.js
@@ -0,0 +1,22 @@
+var opção = prompt('Escolha um número de 1 a 5')
+
+switch (opção) {
+    case "1":
+        console.log ("vc escolheu a opção 1")
+        break
+    case "2":
+        console.log ("vc escolheu a opção 2")
+        break
+    case "3":
+        console.log ("vc escolheu a opção 3")
+        break
+    case "4":
+        console.log ("vc escolheu a opção 4")
+        break
+    case "5":
+        console.log ("vc escolheu a opção 5")
+    break
+    default:
+        console.log("opção inválida")
+        break
+}
