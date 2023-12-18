14ba7a5f77c022b21010ac7782fe3ad06713d158
Thiago Ribeiro Negreiros <thiagoriben@gmail.com>
2023-12-18 11:44:51 -0300

    usando for para criar 3 tabuadas a partir de 1 numero

---
+++ for/3 teste for (3 tabuadas)/script.js
@@ -0,0 +1,15 @@
+let numeroEscolhido = Number(prompt('Escolha um número para ver a tabuada'))
+
+for(let contador = 1; contador <= 10; contador++){
+    console.log(`${numeroEscolhido} x ${contador} = ${numeroEscolhido*contador}`)
+}
+console.log('//////////////////////////')
+numeroEscolhido ++
+for(let contador = 1; contador <= 10; contador++){
+    console.log(`${numeroEscolhido} x ${contador} = ${numeroEscolhido*contador}`)
+}
+console.log('//////////////////////////')
+numeroEscolhido ++
+for(let contador = 1; contador <= 10; contador++){
+    console.log(`${numeroEscolhido} x ${contador} = ${numeroEscolhido*contador}`)
+}
